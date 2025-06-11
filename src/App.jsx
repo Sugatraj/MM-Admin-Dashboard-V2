cb7f3885d0367d749d2fe3e424c722cb4c62e02e
Sugatraj <sugatraj.2106@gmail.com>
2025-06-12 01:36:20 +0530

    Add Profile component and route to App; refactor App for new protected route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Auth from './components/Auth';
-import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
-
+import Dashboard from './components/Dashboard';
+import Profile from './components/Profile';
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +14,7 @@ function App() {
         {/* Protected routes with AppLayout */}
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
           {/* Add other protected routes here */}
         </Route>
       </Routes>
